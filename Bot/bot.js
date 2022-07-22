93fab9c61b1c638d531429b0ccbbe906e4fc1dd6
Evgeniy <kumiro@me.com>
2022-07-23 02:05:43 +0300

    Update bot.js

--- Bot/bot.js
+++ Bot/bot.js
@@ -2,7 +2,6 @@ const { Telegraf } = require("telegraf");
 const TOKEN = "5476521185:AAG1Q2YudVbI_EC04XHSqPME6uvmq8hwdGY";
 const bot = new Telegraf(TOKEN);
 
-//const web_link = "https://telegram-web-k3rb3mjyk-tehnoconductor.vercel.app/";
 const web_link = "https://telegram-web-app-pi.vercel.app/";
 
 bot.start((ctx) =>
